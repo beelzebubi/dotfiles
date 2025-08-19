ec42c966d32a0a764b5fd16bf8540de37f4993e9
beelzebubi <tom@sennewald.me>
2025-08-19 20:13:07 +0200

    updated starship git status

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -674,7 +674,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- ruff = {},
+        ruff = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -734,19 +734,6 @@ require('lazy').setup({
           end,
         },
       }
-
-      -- require('lspconfig').pyright.setup {
-      --   init_options = {},
-      --   },
-      -- }
-
-      -- require('lspconfig').ruff.setup {
-      --   init_options = {
-      --     settings = {
-      --       configuration = nil,
-      --     },
-      --   },
-      -- }
     end,
   },
 
@@ -991,7 +978,7 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
@@ -1001,7 +988,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
--- nvim/dot-config/nvim/lua/custom/plugins/init.lua
+++ nvim/dot-config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  { -- VimTeX for working with TeX
+    'lervag/vimtex',
+    lazy = false, -- we don't want lazy to load VimTeX
+    -- tag = "v2.15 -- uncomment to fix to specific release
+    config = function()
+      vim.g.vimtex_view_method = 'zathura' -- oder skim, okular, sumatrapdf...
+      vim.g.vimtex_compiler_method = 'latexmk'
+    end,
+  },
+}
